4a128f15c86f6f20f9727cf95aa6ef22405677ff
Gary Norman <gary.norman.th@gmail.com>
2024-04-04 19:43:56 +0100

    feat: imported all element IDs as variables in JS

--- js/default.js
+++ js/default.js
@@ -1,3 +1,21 @@
+const operation = document.getElementById("function")
+const standardise = document.getElementById("standardise")
+const colorize = document.getElementById("colorize")
+const output = document.getElementById("output")
+const align = document.getElementById("align")
+const reverse = document.getElementById("reverse")
+const color = document.getElementById("color")
+const red = document.getElementById("red")
+const green = document.getElementById("green")
+const blue = document.getElementById("blue")
+const yellow = document.getElementById("yellow")
+const orange = document.getElementById("orange")
+const standard = document.getElementById("standard")
+const shadow = document.getElementById("shadow")
+const thinkertoy = document.getElementById("thinkertoy")
+const upload = document.getElementById("upload")
+
+ // drag and drop
 function dropHandler(ev) {
     console.log("File(s) dropped");
 
