9e4051ee15745383ce4cb7c7952806e0d34ec11e
Gary Norman <gary.norman.th@gmail.com>
2024-04-11 21:58:50 +0100

    feat: reverse and output functions now working correctly with all banner styles on webserver.go feat: title now serves as link to index feat: gopher is clickable

--- api/processor.go
+++ api/processor.go
@@ -13,36 +13,38 @@ func Processor(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	chosenInput := r.FormValue("generate")
+	inputText := r.FormValue("generate")
 	chosenStyle := r.FormValue("banner")
 	chosenColor := r.FormValue("colors")
-	colorInput := r.FormValue("colour-text")
-	defaultValue := "default"
-	artInput := ascii_art_web.ArtFromFile(w, r)
 	chosenAlign := r.FormValue("text-align")
-	outputResult := pkg.MakeArt(chosenInput, pkg.GetChars(pkg.PrepareBan(chosenStyle)))
-	artOutput := ascii_art_web.CheckReverse(w, r)
-	d := struct {
-		InputText  string
-		Style      string
-		Color      string
-		ColorWord  string
-		FileWant   string
-		InputAlign string
-		CurrentArt string
-		ArtToText  string
-		TextToArt  string
-	}{
-		InputText:  chosenInput,
-		Style:      chosenStyle,
-		Color:      chosenColor,
-		FileWant:   defaultValue,
-		ColorWord:  colorInput,
-		InputAlign: chosenAlign,
-		CurrentArt: artInput,
-		ArtToText:  artOutput,
-		TextToArt:  outputResult,
+	colorWord := r.FormValue("colour-text")
+	artOutput := ""
+	outputResult := pkg.MakeArt(inputText, pkg.GetChars(pkg.PrepareBan(chosenStyle)))
+	artToText := "Your Art:"
+	if ascii_art_web.IsFilePresent(w, r) {
+		artOutput = pkg.Reverse("filetoart/" + Reverse(w, r))
+		artToText = "Your art says: " + artOutput
+		outputResult = pkg.MakeArt(artOutput, pkg.GetChars(pkg.PrepareBan(chosenStyle)))
 	}
+
+	d := struct {
+		InputText   string
+		ChosenStyle string
+		ChosenColor string
+		ColorWord   string
+		ChosenAlign string
+		ArtToText   string
+		TextToArt   string
+	}{
+		InputText:   inputText,
+		ChosenStyle: chosenStyle,
+		ChosenColor: chosenColor,
+		ColorWord:   colorWord,
+		ChosenAlign: chosenAlign,
+		ArtToText:   artToText,
+		TextToArt:   "<pre class=\"" + chosenColor + "\">" + outputResult + "</pre>",
+	}
+
 	tpl.ExecuteTemplate(w, "result.html", d)
 }
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
